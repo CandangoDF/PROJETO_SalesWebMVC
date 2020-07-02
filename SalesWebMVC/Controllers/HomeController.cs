f32d339d5f20eacae17165916cdfa7aeb27ecd6b
WillB <williambatistapereira@hotmail.com>
2020-07-02 09:42:45 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles web MVC App from C# Course";
+            ViewData["Autor"] = "William Batista Pereira";
+            
 
             return View();
         }
